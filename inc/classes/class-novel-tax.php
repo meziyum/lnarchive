6caef637fd831c3a973a0d29714cfb4c8a95e1e1
meziyum <siddhant24feb@gmail.com>
2022-08-07 02:12:48 +0530

    Publisher Taxonomy Class
    Added Publisher Taxonomy to the Novel and Volume post types

--- inc/classes/class-novel-tax.php
+++ inc/classes/class-novel-tax.php
@@ -1,9 +1,6 @@
 <?php
 /**
  * Novel Post Type Taxonomy Class
- * 
- * Status: Progress
- * 
  */
 
 namespace fusfan\inc; //Namespace
@@ -30,58 +27,6 @@ class novel_tax{ //Novel Post Type Taxonomies Class
     }
 
     public function register_novel_taxonomies() { //Register all Novel taxonomies
-
-        register_taxonomy('series', ['novel', 'post'], array(
-            'hierarchical' => false,
-            'labels' => array(
-                'name' => 'Series',
-                'singular_name' => 'Series',
-                'search_items' =>  'Search Series',
-                'all_items' => 'All Series',
-                'parent_item' => 'Parent Series',
-                'parent_item_colon' => 'Parent Series: ',
-                'edit_item' => 'Edit Series',
-                'update_item' => 'Update Series',
-                'add_new_item' => 'Add New Series',
-                'new_item_name' => 'New Series Name',
-                'menu_name' => 'Series',
-                ),
-            //Modify the Taxonomy Slug
-            'rewrite' => array(
-                'slug' => 'series',
-                'with_front' => false, //Hide the base slug that is series
-                'hierarchical' => false
-            ),
-            'show_in_rest' => true, //Show in Guttenburg
-            'show_admin_column' => true,
-            'show_ui' => true,
-        ));
-
-        register_taxonomy('publisher', 'novel', array(
-            'hierarchical' => true,
-            'labels' => array(
-                'name' => 'Publisher',
-                'singular_name' => 'Publisher',
-                'search_items' =>  'Search Publishers',
-                'all_items' => 'All Publishers',
-                'parent_item' => 'Parent',
-                'parent_item_colon' => 'Parent: ',
-                'edit_item' => 'Edit Publisher',
-                'update_item' => 'Update Publisher',
-                'add_new_item' => 'Add New Publisher',
-                'new_item_name' => 'New Publisher Name',
-                'menu_name' => 'Publishers',
-                ),
-            //Modify the Taxonomy Slug
-            'rewrite' => array(
-                'slug' => 'publisher',
-                'with_front' => false, //Hide the base slug that is publisher
-                'hierarchical' => true
-            ),
-            'show_in_rest' => true, //Show in Guttenburg
-            'show_admin_column' => true,
-            'show_ui' => true,
-        ));
     }
 }
 ?>
--- inc/classes/class-novel-volume-tax.php
+++ inc/classes/class-novel-volume-tax.php
@@ -6,9 +6,9 @@
 namespace fusfan\inc; //Namespace
 use fusfan\inc\traits\Singleton; //Singleton Directory using namespace
 
-class novel_volume_tax{ //Novel Post Type Taxonomies Class
+class novel_volume_tax{ //Novel and Volume Post Types Common Taxonomies Class
 
-    use Singleton; //Using Sinlgeton
+    use Singleton; //Using Sinlgetons
 
     protected function __construct(){ //Constructor
 
@@ -27,6 +27,72 @@ class novel_volume_tax{ //Novel Post Type Taxonomies Class
     }
 
     public function register_novel_volume_taxonomies() { //Register all Novel and Volume Common taxonomies
+
+        //Register Publisher Taxonomy
+        register_taxonomy('publisher', ['novel','volume'], array(
+            
+            //All Publisher Labels
+            'labels' => array(
+                'name' => 'Publisher', //General Name
+                'singular_name' => 'Publisher', //Singular Taxonomy Name
+                'search_items' =>  'Search Publisher', //Search
+                'all_items' => 'All Publishers', //List of all
+                'parent_item' => 'Parent Publisher', //Parent
+                'parent_item_colon' => 'Parent Publisher: ', //Parent with colon
+                'name_field_description' => 'Name of the Publisher/Publishing Label', //Desc for name field on edit screen
+                'slug_field_description' => 'The “slug” is the URL-friendly version of the name. It is usually all lowercase and contains only letters, numbers, and hyphens.', //Desc for the slug field
+                'parent_field_description' => 'Assign a publisher if its a publishing label.', //Desc for the Parent field
+                'desc_field_description' => 'A short informational description of the publisher/publishing label', //Desc of the Description field
+                'edit_item' => 'Edit Publisher', //Edit
+                'view_item' => 'View Publisher', //View
+                'update_item' => 'Update Publisher', //Update
+                'add_new_item' => 'Add New Publisher', //Add New
+                'new_item_name' => 'New Publisher Name', //New Item Name
+                'not_found' => 'No publishers found', //Not Found Msg
+                'no_terms' => 'No publishers', //Post and Media tables
+                'filter_by_item' => 'FIlter by Publisher', //Filter msg
+                'most_used' => 'Most Used Publisher', //Most Used Msg
+                'back_to_items' => 'Back to Publishers', //Back to Publishers List
+                'item_link' => 'Publisher Link', //Taxonomy Link in Block Editor
+                'item_link_description' => 'A link to a publisher', //Desc for taxonomy Link in Block Editor
+                'menu_name' => 'Publisher', //Name in Menu
+            ),
+
+            'public' => true, //Public Use
+            'publicly_queryable' => true, //If its for front end
+            'show_ui' => true, //Show Default UI
+            'show_in_menu' => true, //Show in Admin Menu
+            'show_in_nav_menus' => true, //If it can be added to Nav Menus
+            'show_in_rest' => true, //Show in Guttenburg or REST API to be more specific
+            'rest_base' => 'publisher', //Base URL
+            'show_tagcloud' => false, //Tag Cloud Widget
+            'show_in_quick_edit' => false, //Quick Edit
+            'meta_box_cb' => null, //If to use custom callbacks for the taxonomy or default ones (not supported by the Gutenberg Editor)
+            'show_admin_column' => true, //Show Automatic Taxonomy Columns on Post Types
+            'description' => 'A company or label publishing the novels', //Taxonomy Desc
+            'update_count_callback' => '', //Callback for when the taxonomy count is updated
+            'query_var' => 'publisher', //Query name for the wp_query
+            'hierarchical' => true, //Hierarchy
+
+            //Modify the Taxonomy Slug
+            'rewrite' => array(
+                'slug' => 'publisher',
+                'with_front' => false, //Hide the base slug
+                'hierarchical' => false, //If to display hierarchy in the url
+            ),
+
+            //Capabilities
+            'capabilities' => array(
+                'manage_terms' => 'manage_categories',
+                'edit_terms' => 'manage_categories',
+                'delete_terms' => 'manage_categories',
+                'assign_terms' => 'manage_categories',
+            ),
+
+            'sort' => false, //Whether this taxonomy should remember the order in which terms are added to objects
+            '_builtin' => false //IF native or build in taxonomy(Only for Core Development)
+
+        ));//End of Publisher Taxonomy
 
         //Register Genre Taxonomy
         register_taxonomy('genre', ['novel','volume'], array(
@@ -81,7 +147,7 @@ class novel_volume_tax{ //Novel Post Type Taxonomies Class
                 'hierarchical' => false, //If to display hierarchy in the url
             ),
 
-            //Capabilities for Genre Actions
+            //Capabilities
             'capabilities' => array(
                 'manage_terms' => 'manage_categories',
                 'edit_terms' => 'manage_categories',

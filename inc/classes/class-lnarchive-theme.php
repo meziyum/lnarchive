99215a791f333b44762cb8e6fb80d446ba8d68f7
meziyum <siddhant24feb@gmail.com>
2022-10-22 00:28:26 +0530

    Main Theme Class Proper Documentation

--- inc/classes/class-lnarchive-theme.php
+++ inc/classes/class-lnarchive-theme.php
@@ -59,63 +59,27 @@ class lnarchive_theme{ //LNarchive Theme Class
 
      public function setup_theme() { //Main Setup Theme
 
-         add_theme_support( 'align-wide'); //Wide Alignment for Blocks
-         add_theme_support( 'custom-logo', [
-            'header-text'          => array( 'site-title', 'site-description' ) //Replace Title/Desc by Logo
-         ]); //Custom Logo
-
-         add_theme_support('widgets-block-editor'); //Widgets Blocks Editor
-
-         
-
-         $args = array(
+         add_theme_support( 'align-wide' ); //Wide Alignment for Blocks
+         add_theme_support( 'custom-background', array( //Custom Background
             'default-color' => '3a7de8',
-        );
-
-         add_theme_support( 'custom-background', $args ); //Custom Background
+            )
+         );
+         add_theme_support( 'custom-logo', [
+            'header-text'          => array( 'site-title', 'site-description' ), //Replace Title/Desc by Logo
+         ]); //Custom Logo
+         add_theme_support( 'customize_selective_refresh_widgets' ); //Selective Refresh Support for Widgets
+         add_theme_support('widgets-block-editor'); //Widgets Blocks Editor
          add_theme_support( 'post-thumbnails'); //Post Thumbnails
+         add_theme_support('widgets'); //Add Widgets support
          
 
          //Register Image Sizes
          add_image_size('featured-thumbnail', 350, 300, true); //Thumbnail Size
          add_image_size('novel-cover', 1240, 1748, true); //Novel Cover Size
-
-         add_theme_support('widgets'); //Add Widgets Theme
-
-         add_theme_support( 'customize_selective_refresh_widgets' );
      
-         add_theme_support( 'html5', array(
-            'comment-list', 
-            'comment-form',
-            'search-form',
-            'gallery',
-            'caption',
-            'script',
-            'style',
-         ) );
-
-         add_theme_support('wp-block-styles');
-
-         
-
-         add_theme_support( 'editor-styles' );
-
-         add_editor_style('assets/build/css/editor.css'); //Custom Editor
-
-         register_term_meta('publisher', 'publisher_meta_title_val', array(
-            'object_subtype' => '', //Object Subtype
-            'type' => 'string', //The datatype
-            'description' => 'The meta title of the Publisher', //Desc
-            'single' => true, //Whether it ahs one or multiple values per object
-            'default' => '', //Default Value
-            'sanitize_callback' => '', //Callback when sanitixing the meta_key value
-            'auth_callback' => '', //The callback function for the add, edit and delete meta
-            'show_in_rest'=> true, //Whether to Show in Rest APIs
-         ));
-     
-         global $content_width;
-         if( ! isset( $content_width) ) {
-            $content_width=1240;
+         global $content_width; //Global Content Width Variable
+         if( ! isset( $content_width) ) { //If $content_width is not set
+            $content_width=1240; //Set Default Content Width
          }
       }
 

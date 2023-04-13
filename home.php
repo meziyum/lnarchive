8cc7335fdb0324915a5689323b79a912ad5bfd5b
meziyum <siddhant24feb@gmail.com>
2023-04-13 18:53:27 +0530

    Removed Unnecessary Comments

--- home.php
+++ home.php
@@ -7,52 +7,46 @@
 get_header(); //Get the Header
 ?>
 
-<main id="main" class="main-content" role="main"> <!-- Main Content Container -->
-    <div class="row main-row"> <!-- Main Row -->
-        <div class="blog-wrap col-lg-9"> <!-- Blog Content Div -->
+<main id="main" class="main-content" role="main">
+    <div class="row main-row">
+        <div class="blog-wrap col-lg-9">
             <?php
-
-            //Title
             printf(
-                '<h1 class="page-title">%1$s</h1>', //Page Title Div
-                wp_kses_post( get_the_title(get_option('page_for_posts', true))) //Get the Title
+                '<h1 class="page-title">%1$s</h1>',
+                wp_kses_post(get_the_title(get_option('page_for_posts', true)))
             );
-            
-            if(have_posts()) { //If there is post
-                post_list($wp_query, 'post-blog'); //Post List
+
+            if(have_posts()) {
+                post_list($wp_query, 'post-blog');
             }
 
-            //HTML tags allowed inside the wp_kses
             $allowed_tags = [
-                'button' => [ //Span Div
-                    'class' => [], //Class
+                'button' => [
+                    'class' => [],
                 ],
-                'span' => [ //Span
-                    'class' =>[], //Class
+                'span' => [
+                    'class' =>[],
                 ],
-                'a' => [ //A div
-                    'class' => [], //Class
-                    'href' => [], //Href
+                'a' => [
+                    'class' => [],
+                    'href' => [],
                 ]
             ];
 
-            //Pagination Function Arguments
             $args = [
-                'before_page_number' => '<button class="blog-page-no pagination-button">', //HTML before page no
-                'after_page_number' => '</button>', //HTML after page no
-                'prev_text' => '<button class="blog-page-prev pagination-button">' . '«' . '</button>', //HTML for next button
-                'next_text' => '<button class="blog-page-next pagination-button">' . '»' . '</button>', //HTML for prev button
+                'before_page_number' => '<button class="blog-page-no pagination-button">',
+                'after_page_number' => '</button>',
+                'prev_text' => '<button class="blog-page-prev pagination-button">' . '«' . '</button>',
+                'next_text' => '<button class="blog-page-next pagination-button">' . '»' . '</button>',
             ];
 
-            //Display the Pagination
-            printf( '<nav class="blog-links d-flex justify-content-center">%s</nav>', wp_kses( paginate_links( $args), $allowed_tags ));
-        ?>
+            printf('<nav class="blog-links d-flex justify-content-center">%s</nav>', wp_kses(paginate_links($args), $allowed_tags));
+            ?>
         </div>
-        <aside class="sidebar-wrap col d-none d-lg-block"> <!-- Sidebar Div -->
-            <?php get_sidebar('sidebar-main'); //Show the Sidebar?>
+        <aside class="sidebar-wrap col d-none d-lg-block">
+            <?php get_sidebar('sidebar-main'); ?>
         </aside>
     </div>
 </main>
 
-<?php get_footer(); //Get the Footer ?>
-
+<?php get_footer(); ?>

ace32c64979d10b338481ff17f539a1cab9c1394
meziyum <siddhant24feb@gmail.com>
2023-05-01 08:29:22 +0530

    Search Bar Overflow into Profile Menu Bug Fix

--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -12,7 +12,7 @@ $header_menu_id = $menu_class->get_menu_id('fusfan_primary');
 $header_menus = wp_get_nav_menu_items( $header_menu_id );
 ?>
 
-<nav class="header-nav navbar navbar-expand-lg navbar-light bg-light fixed-top">
+<nav class="header-nav navbar navbar-expand-lg">
   <div class="container">
  
     <div class="navbar-brand">
@@ -28,94 +28,91 @@ $header_menus = wp_get_nav_menu_items( $header_menu_id );
     </button>
 
     <div class="collapse navbar-collapse row" id="navbarSupportedContent">
+      <div id="profile-menu" class="col-lg-1 order-lg-3">
+        <?php
+          if( is_user_logged_in() ) {
 
-    <div class="profile d-flex col-lg-1 order-lg-3 d-flex justify-content-center">
-
-      <?php
-        if( is_user_logged_in() ) {
-
-          $args = array(
-            'height' => 96,
-            'width' => 96,
-            'force_default' => false,
-            'rating' => 'X',
-            'class' => 'profile-pic',
-            'force_display' => true,
-            'loading' => 'eager',
-            'extra_attr' => '',
-          );
-          ?>
-              
-          <div class="dropdown">
-            <a href="#" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-              <?php
-                echo get_avatar( 
-                get_current_user_id(), 
-                96,
-                '',
-                'Profile Pic',
-                $args,
-                );
-              ?>
-            </a>
-            <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end" aria-labelledby="dropdownMenuLink">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-            </ul>
-          </div>
-          <?php
-        }
-      ?>
+            $args = array(
+              'height' => 96,
+              'width' => 96,
+              'force_default' => false,
+              'rating' => 'X',
+              'class' => 'profile-pic',
+              'force_display' => true,
+              'loading' => 'eager',
+              'extra_attr' => '',
+            );
+            ?>
+                
+            <div class="dropdown">
+              <a href="#" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                <?php
+                  echo get_avatar( 
+                  get_current_user_id(), 
+                  96,
+                  '',
+                  'Profile Pic',
+                  $args,
+                  );
+                ?>
+              </a>
+              <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end" aria-labelledby="dropdownMenuLink">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+              </ul>
+            </div>
+            <?php
+          }
+        ?>
       </div>
 
-    <div class="page-list col-lg-8 col-md-12 order-lg-1">
-      <?php
-        if( ! empty( $header_menus) && is_array( $header_menus )){
-          ?>
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <?php
-                foreach ( $header_menus as $menu_item){
-                  if( ! $menu_item->menu_item_parent) {
+      <div id="main-search" class="col-lg-4 col-md-12 order-lg-2"></div>
 
-                    $child_menu_items = $menu_class->get_child_menu_items( $header_menus, $menu_item->ID);
-                    $has_children = !empty( $child_menu_items) && is_array( $child_menu_items);
+      <div class="page-list col-lg-7 col-md-12 order-lg-1">
+        <?php
+          if( ! empty( $header_menus) && is_array( $header_menus )){
+            ?>
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <?php
+                  foreach ( $header_menus as $menu_item){
+                    if( ! $menu_item->menu_item_parent) {
 
-                    if( !$has_children) {
-                      ?>
-                        <li class="nav-item">
-                          <a class="nav-link active" aria-current="page" href="<?php echo esc_url( $menu_item->url );?>">
+                      $child_menu_items = $menu_class->get_child_menu_items( $header_menus, $menu_item->ID);
+                      $has_children = !empty( $child_menu_items) && is_array( $child_menu_items);
+
+                      if( !$has_children) {
+                        ?>
+                          <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="<?php echo esc_url( $menu_item->url );?>">
+                              <?php echo esc_html( $menu_item->title); ?>
+                            </a>
+                          </li>
+                        <?php
+                      } else {
+                        ?>
+                        <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle" href="<?php echo esc_url( $menu_item->url );?>" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             <?php echo esc_html( $menu_item->title); ?>
                           </a>
-                        </li>
-                      <?php
-                    } else {
-                      ?>
-                      <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="<?php echo esc_url( $menu_item->url );?>" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                          <?php echo esc_html( $menu_item->title); ?>
-                        </a>
-                        
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <?php
-                              foreach( $child_menu_items as $child_menu_item) {
+                          
+                          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                              <?php
+                                foreach( $child_menu_items as $child_menu_item) {
+                                  ?>
+                                    <li><a class="dropdown-item" href="<?php echo esc_url( $child_menu_item->url)?>">
+                                      <?php echo esc_html( $child_menu_item->title); ?>
+                                    </a></li>
+                                    <?php
+                                }
                                 ?>
-                                  <li><a class="dropdown-item" href="<?php echo esc_url( $child_menu_item->url)?>">
-                                    <?php echo esc_html( $child_menu_item->title); ?>
-                                  </a></li>
-                                  <?php
-                              }
-                              ?>
-                        </ul>
-                      </li><?php
+                          </ul>
+                        </li><?php
+                      }
                     }
-                  }
-                }?>
-            </ul><?php
-        }
-      ?>
-      </div>
-      
-      <div id="main-search" class="col-lg-3 col-md-12 order-lg-2">
+                  }?>
+              </ul><?php
+          }
+        ?>
       </div>
     </div>
   </div>

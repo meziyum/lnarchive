d63d586754669ca958032f9bf09ee0b8d02f7188
meziyum <siddhant24feb@gmail.com>
2023-05-03 12:40:02 +0530

    Novels Cover Fixed Ratio #58

--- home.php
+++ home.php
@@ -9,6 +9,7 @@ get_header();
 
 <main id="main" class="main-content" role="main">
     <div class="row main-row">
+        <h1 class="page-title">Blog</h1>
         <div id="blog-wrap"></div>
     </div>
 </main>

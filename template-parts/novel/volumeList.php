1851cfe61f402d698b6a0247da9daab50355ddec
meziyum <siddhant24feb@gmail.com>
2023-05-07 05:58:58 +0530

    Display Volumes List only when at least 1 volume added to a novel

--- template-parts/novel/volumeList.php
+++ template-parts/novel/volumeList.php
@@ -18,12 +18,14 @@ $vol_args = array(
 
 $vquery = new WP_Query($vol_args);
 
-?>
-    <section id="volumes-section" class="novels-list-section">
-        <h2 id="volumes-no">Volumes</h2>
-        <?php novel_list( $vquery, array( 'name' => 'volume'));?>
-    </section>
-<?php
+if($vquery->have_posts()) {
+    ?>
+        <section id="volumes-section" class="novels-list-section">
+            <h2 id="volumes-no">Volumes</h2>
+            <?php novel_list( $vquery, array( 'name' => 'volume'));?>
+        </section>
+    <?php
+}
 
 wp_reset_query();
 ?>

bc74f988ec06b61d0ed93b31a49a1787c2d461b0
meziyum <siddhant24feb@gmail.com>
2023-05-14 16:59:07 +0530

    No Cover Novels not loading bug fix

--- src/js/pages/library/Components/NovelArchive.jsx
+++ src/js/pages/library/Components/NovelArchive.jsx
@@ -103,9 +103,11 @@ function NovelArchive(props) {
             );
 
             const data= await response.json();
+            console.log(data)
 
             const novels = data.map( (novel) => {
-                const novelCover=novel._embedded['wp:featuredmedia'] ? novel._embedded['wp:featuredmedia'][0].source_url : null;
+                console.log(novel)
+                const novelCover=novel._embedded ? novel._embedded['wp:featuredmedia'][0].source_url : null;
                 return (
                     <NovelItem key={novel.id} id={novel.id} title={formatTitle(novel.title.rendered)} link={novel.link} novelCover={novelCover} popularity={novel.meta.popularity[0]} rating={novel.meta.rating[0]}/>
                 );

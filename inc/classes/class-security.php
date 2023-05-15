51fa3e651dd5d5c0ea074465a8d535f5e21e9f09
meziyum <siddhant24feb@gmail.com>
2023-05-15 14:00:29 +0530

    Remove Security to hide login error messages

--- inc/classes/class-security.php
+++ inc/classes/class-security.php
@@ -16,16 +16,11 @@ class security{
 
     protected function set_hooks() {
         add_filter('the_generator', [$this, 'security_remove_version']);
-        add_filter( 'login_errors', [$this, 'no_wordpress_errors'] );
         add_filter('xmlrpc_enabled', [ $this, '__return_false']);
     }
 
     function security_remove_version() {
         return '';
-    }
-
-    function no_wordpress_errors() {
-        return 'Your username or password is incorrect';
     }
 }
 ?>

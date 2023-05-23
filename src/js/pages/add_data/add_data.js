ad2fd5884ef320dc4a34f742532c32c1d1745afc
meziyum <siddhant24feb@gmail.com>
2023-05-23 21:44:12 +0530

    Add Data Page js

--- src/js/pages/add_data/add_data.js
+++ src/js/pages/add_data/add_data.js
@@ -3,3 +3,27 @@ import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import '../../../sass/add_data/add_data.scss';
 import '../common.js';
+import AddDataForm from './Components/AddDataForm.tsx';
+
+/* eslint-disable no-undef */
+const customAPIRequestURL = lnarchiveVariables.custom_api_url;
+/* eslint-enable no-undef */
+const addDataRoot = ReactDOMClient.createRoot(document.getElementById('add-data-main'));
+
+const novelResponse = await fetch(`${customAPIRequestURL}novel_filters`, {
+    method: 'GET',
+    credentials: 'same-origin',
+    headers: {
+        'Content-Type': 'application/json',
+    }});
+const novelFilters = await novelResponse.json();
+
+const volumeResponse = await fetch(`${customAPIRequestURL}volume_filters`, {
+    method: 'GET',
+    credentials: 'same-origin',
+    headers: {
+        'Content-Type': 'application/json',
+    }});
+const volumeFilters = await volumeResponse.json();
+
+addDataRoot.render(<AddDataForm novelFilters={novelFilters} volumeFilters={volumeFilters}/>);

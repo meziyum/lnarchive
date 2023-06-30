45990d71a76ebc3dc2d8ff77958a0b7be8a6fd2d
meziyum <siddhant24feb@gmail.com>
2023-06-30 12:09:25 +0530

    get_user_subscription_status Function #91

--- inc/helpers/user.php
+++ inc/helpers/user.php
@@ -11,4 +11,15 @@
         $table_name = $wpdb->prefix . 'user_ratings';
         return $wpdb->get_var("SELECT rating FROM $table_name WHERE object_id=".$comment['post']." AND user_id=".$comment['author']);
     }
+
+    function get_user_subscription_status($user_id, $object_id) {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'user_subscriptions';
+        $already_subscribed = $wpdb->get_var("SELECT subscription_id FROM $table_name WHERE user_id=$user_id AND object_id = $object_id");
+
+        if ($already_subscribed) {
+            return true; 
+        }
+        return false;
+    }
 ?>

32e37c06db6a4861862cebcbe8972a5593cc2820
meziyum <siddhant24feb@gmail.com>
2023-07-03 20:58:55 +0530

    Get reading lists and the novel status on the lists

--- inc/helpers/user.php
+++ inc/helpers/user.php
@@ -6,20 +6,19 @@
 * @package LNarchive
 */
 
-    function get_user_rating($comment) {
+    function get_reading_lists_with_novel_status($user_id, $novel_id) {
         global $wpdb;
-        $table_name = $wpdb->prefix . 'user_ratings';
-        return $wpdb->get_var("SELECT rating FROM $table_name WHERE object_id=".$comment['post']." AND user_id=".$comment['author']);
-    }
+        $table_name = $wpdb->prefix . 'reading_list';
+        $reading_lists = $wpdb->get_results("SELECT list_id, name FROM $table_name WHERE user_id=$user_id", 'ARRAY_A');
 
-    function get_user_subscription_status($user_id, $object_id) {
-        global $wpdb;
-        $table_name = $wpdb->prefix . 'user_subscriptions';
-        $already_subscribed = $wpdb->get_var("SELECT subscription_id FROM $table_name WHERE user_id=$user_id AND object_id = $object_id");
-
-        if ($already_subscribed) {
-            return true; 
+        for($i=0; $i<count($reading_lists); $i++) {
+            if (is_present_in_reading_list($novel_id, $reading_lists[$i]['list_id'])) {
+                $reading_lists[$i]['present'] = 1;
+            } else {
+                $reading_lists[$i]['present'] = 0;
+            }
         }
-        return false;
+        error_log(print_r($reading_lists, true));
+        return $reading_lists;
     }
 ?>

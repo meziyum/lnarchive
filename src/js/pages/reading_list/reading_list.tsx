846d4eae799a09d52785da754fb22570f1eef6dd
meziyum <siddhant24feb@gmail.com>
2023-07-04 15:23:16 +0530

    Remove Unnecessary Variables

--- src/js/pages/reading_list/reading_list.tsx
+++ src/js/pages/reading_list/reading_list.tsx
@@ -5,14 +5,11 @@ import '../../../sass/reading_list/reading_list.scss';
 import '../common.js';
 import ReadingList from './Components/ReadingList';
 
-/* eslint-disable no-undef */
-const customAPIRequestURL = lnarchiveVariables.custom_api_url;
-/* eslint-enable no-undef */
 const readingListDOM = document.getElementById('reading-list-section');
 
 if (readingListDOM) {
     const readingListRoot = ReactDOMClient.createRoot(readingListDOM);
     readingListRoot.render(<ReadingList/>);
 } else {
-    throw new Error('Unable to Locate the Root to render the components')
+    throw new Error('Unable to Locate the Root to render the components');
 }

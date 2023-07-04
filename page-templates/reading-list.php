6ac282cbb73444550c28626c64905170fe4a891c
meziyum <siddhant24feb@gmail.com>
2023-07-04 14:46:56 +0530

    Must Have Query Parameter

--- page-templates/reading-list.php
+++ page-templates/reading-list.php
@@ -5,8 +5,16 @@
  * @package LNarchive
  */
 get_header();
-?>
 
+if (!isset($_GET['list_id'])) {
+    global $wp_query;
+    $wp_query->set_404();
+    status_header(404);
+    nocache_headers();
+    include(get_404_template());
+    exit;
+}
+?>
 <main id="main" class="main-content" role="main">
     <div id="<?php echo esc_attr(get_the_ID());?>" class="main-row">
         <div class="reading-list-wrap">

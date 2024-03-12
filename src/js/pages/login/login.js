3ec9040a4bd12b48aca8bebffcd5e20be6e64d95
meziyum <siddhant24feb@gmail.com>
2024-03-12 20:42:42 +0530

    Auto Placeholder values for Registration Screen
    Automatically add placeholder values to the registration form using dynamic js after the DOM is loaded

--- src/js/pages/login/login.js
+++ src/js/pages/login/login.js
@@ -1,2 +1,7 @@
 
 import '../../../sass/login/login.scss';
+
+window.addEventListener('DOMContentLoaded', function() {
+    document.getElementsByName('user_login')[0].placeholder='Enter desired username';
+    document.getElementsByName('user_email')[0].placeholder='Enter Email';
+});
